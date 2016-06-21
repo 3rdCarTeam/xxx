1162969bc5d0557694f41ce7db4ef6ac6b920077
“hiyodo” <a7689999@126.com>
2016-06-21 16:12:23 +0800

    fuuck u

---
+++ 打地鼠/打地鼠/打地鼠/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  打地鼠
+//
+//  Created by tarenayj on 16/5/10.
+//  Copyright © 2016年 TR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 打地鼠/打地鼠/打地鼠/myViewController.h
@@ -0,0 +1,13 @@
+//
+//  myViewController.h
+//  打地鼠
+//
+//  Created by tarenayj on 16/5/10.
+//  Copyright © 2016年 TR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface myViewController : UIViewController
+
+@end
