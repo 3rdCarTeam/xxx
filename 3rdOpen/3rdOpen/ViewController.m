4d3d2135de4fa651a941484c23267614a62919e8
“hiyodo” <a7689999@126.com>
2016-06-21 11:47:36 +0800

    创建工程

---
+++ 3rdOpen/3rdOpen/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  3rdOpen
+//
+//  Created by tarenayj on 16/6/21.
+//  Copyright © 2016年 TR. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
